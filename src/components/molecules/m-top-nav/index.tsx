93ed226c8be8a0ac897384b5081d9080ee13d4bf
Ezekiel Victor <codeepoch@gmail.com>
2024-11-14 12:47:44 +0100

    improve search mobile responsivness

--- src/components/molecules/m-top-nav/index.tsx
+++ src/components/molecules/m-top-nav/index.tsx
@@ -8,6 +8,9 @@ import LineMdMenuToCloseTransition from "~icons/line-md/menu-to-close-transition
 import { useAtom } from "jotai";
 import navAtom from "@/state/atoms/navAtom";
 import UserNavAvatarInfo from "@/components/atoms/a-user-nav-avatar-info";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
+import IonSearch from "~icons/ion/search.jsx";
+import { AnimatePresence, motion } from "framer-motion";
 
 const TopNav = () => {
   const [navOpen, setNavOpen] = useAtom(navAtom);
@@ -31,6 +34,30 @@ const TopNav = () => {
       </div>
 
       <div className={styles.rightSm}>
+        <Popover>
+          {({ open }) => (
+            <div>
+              <PopoverButton className={styles.searchButton}>
+                <IonSearch />
+              </PopoverButton>
+              <AnimatePresence>
+                {open && (
+                  <PopoverPanel
+                    static
+                    as={motion.div}
+                    initial={{ opacity: 0, scale: 0.95 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0, scale: 0.95 }}
+                    anchor="bottom end"
+                    className={styles.searchPannel}
+                  >
+                    <SearchInput />
+                  </PopoverPanel>
+                )}
+              </AnimatePresence>
+            </div>
+          )}
+        </Popover>
         <button onClick={() => setNavOpen((c) => !c)}>
           {navOpen ? (
             <LineMdMenuToCloseTransition />
